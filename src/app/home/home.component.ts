f32489871bab31cefb3130e6b5b6c3465201fe35
Jasiva <sivakumar.yb@gmail.com>
2023-10-16 23:59:33 +0530

    creating webpage

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = [];
+  cardsForHandset = [];
+  cardsForWeb = [];
+
+  isHandset: boolean = false;
+  isHandsetObserver: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(({ matches }) => {
+        if (matches) {
+          return true;
+        }
+
+        return false;
+      })
+    );
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.isHandsetObserver.subscribe((currentObserverValue) => {
+      this.isHandset = currentObserverValue;
+      this.loadCards();
+    });
+  }
+
+  loadCards() {
+    this.cards = this.isHandset ? this.cardsForHandset : this.cardsForWeb;
+  }
+}
